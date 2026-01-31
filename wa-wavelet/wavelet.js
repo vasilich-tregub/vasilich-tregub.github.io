8c04bf0c805586d17397148a8722aee7a3e1cf7b
V.V.T. <vasilich.tregub@gmail.com>
2026-01-31 17:20:43 +0700

    LeGall 5/3 wavelet

---
+++ wa-wavelet/wavelet.js
@@ -0,0 +1,115 @@
+/* TERMS OF USE
+ * This source code is subject to the terms of the MIT License. 
+ * Copyright(c) 2026 Vladimir Vasilich Tregub
+*/
+var imR;
+var imG;
+var imB;
+var imageData;
+function forward_transform() {
+    const img = document.getElementById("idImgSrc");
+    document.getElementById("idCanvas").width = img.naturalWidth;
+    document.getElementById("idCanvas").height = img.naturalHeight;
+    const ctx = document.getElementById("idCanvas").getContext("2d", { willReadFrequently: true });
+    ctx.drawImage(img, 0, 0);
+    imageData = ctx.getImageData(0, 0, img.naturalWidth, img.naturalHeight);
+    // packed to planar color representation, and dwt for each bitplane
+    imR = new Int32Array(new ArrayBuffer(img.naturalWidth * img.naturalHeight * 4));
+    imG = new Int32Array(new ArrayBuffer(img.naturalWidth * img.naturalHeight * 4));
+    imB = new Int32Array(new ArrayBuffer(img.naturalWidth * img.naturalHeight * 4));
+    for (let ih = 0; ih < idCanvas.height; ++ih) {
+        for (let iw = 0; iw < idCanvas.width; ++iw) {
+            imR[ih * imageData.width + iw] = imageData.data[(ih * imageData.width + iw) * 4 + 0] << 12;
+            imG[ih * imageData.width + iw] = imageData.data[(ih * imageData.width + iw) * 4 + 1] << 12;
+            imB[ih * imageData.width + iw] = imageData.data[(ih * imageData.width + iw) * 4 + 2] << 12;
+        }
+        dwt_forward(imR, ih * imageData.width, imageData.width, 0);
+        dwt_forward(imR, ih * imageData.width, imageData.width, 1);
+        dwt_forward(imG, ih * imageData.width, imageData.width, 0);
+        dwt_forward(imG, ih * imageData.width, imageData.width, 1);
+        dwt_forward(imB, ih * imageData.width, imageData.width, 0);
+        dwt_forward(imB, ih * imageData.width, imageData.width, 1);
+        for (let iw = 0; iw < idCanvas.width; ++iw) {
+            imageData.data[(ih * imageData.width + iw) * 4 + 0] = (imR[ih * imageData.width + iw] >> 12) + 128;
+            imageData.data[(ih * imageData.width + iw) * 4 + 1] = (imG[ih * imageData.width + iw] >> 12) + 128;
+            imageData.data[(ih * imageData.width + iw) * 4 + 2] = (imB[ih * imageData.width + iw] >> 12) + 128;
+        }
+    }
+    ctx.putImageData(imageData, 0, 0);
+}
+function inverse_transform() {
+    const ctx = document.getElementById("idCanvas").getContext("2d", { willReadFrequently: true });
+    const img = document.getElementById("idImgSrc");
+    //imageData = ctx.getImageData(0, 0, img.naturalWidth, img.naturalHeight);
+    for (let ih = 0; ih < idCanvas.height; ++ih) {
+        dwt_inverse(imR, ih * imageData.width, imageData.width, 1);
+        dwt_inverse(imR, ih * imageData.width, imageData.width, 0);
+        dwt_inverse(imG, ih * imageData.width, imageData.width, 1);
+        dwt_inverse(imG, ih * imageData.width, imageData.width, 0);
+        dwt_inverse(imB, ih * imageData.width, imageData.width, 1);
+        dwt_inverse(imB, ih * imageData.width, imageData.width, 0);
+        for (let iw = 0; iw < idCanvas.width; ++iw) {
+            imageData.data[(ih * imageData.width + iw) * 4 + 0] = imR[ih * imageData.width + iw] >> 12;
+            imageData.data[(ih * imageData.width + iw) * 4 + 1] = imG[ih * imageData.width + iw] >> 12;
+            imageData.data[(ih * imageData.width + iw) * 4 + 2] = imB[ih * imageData.width + iw] >> 12;
+        }
+    }
+    ctx.putImageData(imageData, 0, 0);
+}
+function dwt_forward(im, beg, len, level) {
+    const inc = 1 << level;
+	const end = beg + len;
+    //assert(inc < end && "stepping outside source image");
+
+	let i = beg + inc;
+    // high pass filter, {-1./2, 1., -1./2}
+    for (; i < end - inc; i += 2 * inc) {
+        im[i] -= (im[i - inc] + im[i + inc]) >> 1;
+    }
+    if (i < end) {
+        im[i] -= im[i - inc];
+    }
+
+    i = beg;
+    // low pass filter, 
+    // successive convolutions with {-1./2, 1., -1./2} for odd pixels
+    // and {1./4, 1., 1./4} for even pixels
+    // for im[n] result is -im[n-2]/8 + im[n-1]/4 + 6*im[n]/8 + im[n+1]/4 - im[n+2]/8
+    // i.e., {-1./8, 2./8, 6./8, 2./8, -1./8}
+    im[i] += (im[inc] + 1) >> 1;
+    i += 2 * inc;
+    for (; i < end - inc; i += 2 * inc) {
+        im[i] += (im[i - inc] + im[i + inc] + 2) >> 2;
+    }
+    if (i < end) {
+        im[i] += (im[i - inc] + 1) >> 1;
+    }
+}
+function dwt_inverse(im, beg, len, level) {
+    const inc =  1 << level;
+	const end = beg + len;
+    //assert(inc < end && "stepping outside source image");
+
+	// low pass filter, {-1./4, 1./4, -1./4}
+	let i = beg;
+    im[i] -= (im[inc] + 1) >> 1;
+    i += 2 * inc;
+    for (; i < end - inc; i += 2 * inc) {
+        im[i] -= (im[i - inc] + im[i + inc] + 2) >> 2;
+    }
+    if (i < end) {
+        im[i] -= (im[i - inc] + 1) >> 1;
+    }
+
+    // high pass filter, {-1./8, 1./8, 6./8, 1./8 -1./8}
+    // successive convolutions with {-1./4, 1., -1./4} for even pixels
+    // and {1./2, 1., 1./2} for even pixels
+    // for im[n] result is -im[n-2]/8 + im[n-1]/8 + 6*im[n]/8 + im[n+1]/8 - im[n+2]/8
+    i = beg + inc;
+    for (; i < end - inc; i += 2 * inc) {
+        im[i] += (im[i - inc] + im[i + inc]) >> 1;
+    }
+    if (i < end) {
+        im[i] += im[i - inc];
+    }
+}
