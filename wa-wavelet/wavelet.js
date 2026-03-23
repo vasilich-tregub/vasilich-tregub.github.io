5f6cfb926e2b3d26f430b97cd9a4c74aa2d45a01
V.V.T. <vasilich.tregub@gmail.com>
2026-03-23 17:24:31 +0700

    terms of use

--- wa-wavelet/wavelet.js
+++ wa-wavelet/wavelet.js
@@ -1,3 +1,7 @@
+/* TERMS OF USE
+ * This source code is subject to the terms of the MIT License. 
+ * Copyright(c) 2026 Vladimir Vasilich Tregub
+ */
 var waobj;
 var decompmem;
 WebAssembly.instantiateStreaming(
