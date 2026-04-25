a55da493f3ed0b7056548a91d3d4e55678d693b8
V.V.T. <vasilich.tregub@gmail.com>
2026-04-25 22:30:31 +0700

    animation for would-be streaming demo

--- stream-animation/canvas.js
+++ stream-animation/canvas.js
@@ -1,10 +1,28 @@
 var marginX = 0;
-const speed = 30;
+var text1 = "";
+var text2 = "";
+var speed = 30;
 const deltaW = 0.9;
 var changeSign = -1;
 var intervalId = 0;
 var animInProgress = false;
-window.onload = () => { colorBackground("#6a5acd"); marginX = Number(margin.value);}
+window.onload = () => {
+    colorBackground("#6a5acd");
+    marginX = Number(margin.value);
+    text1 = idText1.value;
+    text2 = idText2.value;
+    speed = Number(idSpeed.value);
+}
+function setTexts() {
+    text1 = idText1.value;
+    text2 = idText2.value;
+}
+function setMargin() {
+    marginX = Number(margin.value);
+}
+function setSpeed() {
+    speed = Number(idSpeed.value);
+}
 function clearCanvas() {
     const ctx = document.getElementById("canvas").getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -13,6 +31,7 @@ function animateColumnWidth() {
     if (animInProgress) {    
         clearInterval(intervalId);
         animInProgress = false;
+        speed = Number(idSpeed.value);
     }
     else {    
         intervalId = setInterval(drawText, speed);
@@ -25,7 +44,7 @@ function drawText() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let fontsz = document.getElementById("fontsize").value;
     ctx.font = fontsz + "px serif";
-    const text1 = "Dynamic column width text layout demo";
+    //const text1 = "Dynamic column width text layout demo";
     const words = text1.split(/\s/);    
     let xpos = marginX;
     let ypos = 100;
@@ -38,7 +57,7 @@ function drawText() {
         drawTextRun(words[ix], xpos, ypos);
         xpos += metrics.width;
     }
-    const text2 = "عرض توضيلتخطيط النص بعرض أعمدة ديناميكي"
+    //const text2 = "عرض توضيلتخطيط النص بعرض أعمدة ديناميكي"
     const words2 = text2.split(/\s/);
     xpos = canvas.width - marginX;
     ypos += ctx.measureText(words2[0]).fontBoundingBoxAscent + ctx.measureText(words2[0]).fontBoundingBoxDescent;
