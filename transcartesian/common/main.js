b479e2c5b7acfaaab7e0c342bd3a970fb4a915ff
V.V.T. <vasilich.tregub@gmail.com>
2026-07-18 22:14:01 +0700

    map a min-neighbor mesh to 2D for visuals

--- transcartesian/common/main.js
+++ transcartesian/common/main.js
@@ -12,6 +12,10 @@ function s4(int) {
 var rank = 64;
 var a;
 var mesh = new Map(); // map item: key is index.toString(4); value is an array of node neighbors, 3 neighbors or less
+var kernel2d = [];
+var kernel2dr = [];
+var mesh2d = [];
+var mesh2dr = [];
 var neighmeshes = new Set();
 var meshBoundary = new Map();
 function connectNodes(node1key, node2key) { 
@@ -91,6 +95,31 @@ function addBorderNeighbors() {
         mesh.set(neigh, []);
         connectNodes("0", neigh);
     }
+    kernel2d = Array(3);
+    kernel2dr = Array(3);
+    for (let ix = 0; ix < 3; ++ix) {
+        kernel2d[ix] = [];
+        kernel2dr[ix] = [];
+    }
+    mesh.forEach((value, key) => {
+        if (key == "0") {
+            kernel2d[1].push(key);
+        }
+        else if (key == "1") {
+            kernel2d[0].push(key);
+        }
+        else {
+            kernel2d[2].push(key);
+        }
+    });
+    for (let ix = 0; ix < 3; ++ix) {
+        kernel2d[ix].forEach((item) => kernel2dr[2 - ix].push(item));
+    }
+    for (let ix = 0; ix < 3; ++ix) {
+        kernel2dr[ix].reverse();
+    }
+    console.log(kernel2d);
+    console.log(kernel2dr);
     console.log("mesh 0123");
     mesh.forEach(logMapElements);
     let mesh0 = new Map(); // 2-DIGIT KEYS
@@ -116,6 +145,29 @@ function addBorderNeighbors() {
     mesh = new Map([...mesh, ...iter.next().value, ...iter.next().value, ...iter.next().value]);
     addCornerNeighbors(2);
     addBorderNeighbors();
+    let rows2d = 7;
+    mesh2d = Array(rows2d);
+    mesh2dr = Array(rows2d);
+    for (let ix = 0; ix < rows2d; ++ix) {
+        mesh2d[ix] = [];
+        mesh2dr[ix] = [];
+    }
+    kernel2dr[0].forEach((item) => mesh2d[0].push("3" + item));
+    kernel2dr[0].forEach((item) => mesh2d[0].push("2" + item));
+    for (let ix = 1; ix < 3; ++ix) {
+        kernel2dr[ix].forEach((item) => mesh2d[ix].push("3" + item));
+        kernel2d[ix - 1].forEach((item) => mesh2d[ix].push("0" + item));
+        kernel2dr[ix].forEach((item) => mesh2d[ix].push("2" + item));
+    }
+    kernel2d[2].forEach((item) => mesh2d[3].push("0" + item));
+    for (let ix = 4; ix < rows2d; ++ix) {
+        kernel2dr[ix - 4].forEach((item) => mesh2d[ix].push("1" + item));
+    }
+    for (let ix = 0; ix < rows2d; ++ix) {
+        mesh2d[ix].forEach((item) => mesh2dr[rows2d - 1 - ix].push(item));
+    }
+    console.log(mesh2d);
+    console.log(mesh2dr);
     console.log("merged '2-digit-key' mesh of " + mesh.size + " nodes; 2-digit-key gives (parseInt('33', 4) + 1) nodes");
     mesh.forEach(logMapElements);
     console.log("Total " + mesh.size + " nodes");
